00feb0d58855d31ed97c3d0eeb0a201f9b44d69d
martinrfh <47691971+martinrfh@users.noreply.github.com>
2020-09-30 18:20:36 +0330

    Updates

---
+++ precache-manifest.f6ba25fe95cf53807a7f038dd0f2bbe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0617e084d67f6ece360f34e4a6c96755",
+    "url": "/youtube-downloader/index.html"
+  },
+  {
+    "revision": "fa572131212d8aa6da7f",
+    "url": "/youtube-downloader/static/css/main.5826c1ac.chunk.css"
+  },
+  {
+    "revision": "8cac04186d047ae2d953",
+    "url": "/youtube-downloader/static/js/2.2fc25f47.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/youtube-downloader/static/js/2.2fc25f47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa572131212d8aa6da7f",
+    "url": "/youtube-downloader/static/js/main.9ed10830.chunk.js"
+  },
+  {
+    "revision": "396850f76d09a1b52a4d",
+    "url": "/youtube-downloader/static/js/runtime-main.568faac1.js"
+  },
+  {
+    "revision": "565fbb54f0b254b8d1da00b4776ba567",
+    "url": "/youtube-downloader/static/media/youtube.565fbb54.png"
+  }
+]);
